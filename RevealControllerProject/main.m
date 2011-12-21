c4176bbd096a35f07bbd098cc93cac5c1ae8e9dd
Philip Kluz <Philip.Kluz@gmail.com>
2011-12-21 07:39:37 +0100

    License fix.

--- RevealControllerProject/main.m
+++ RevealControllerProject/main.m
@@ -1,6 +1,6 @@
 /* 
  
- Copyright (c) 2011, zuui.org (Philip Kluz)
+ Copyright (c) 2011, Philip Kluz (Philip.Kluz@zuui.org)
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
@@ -13,14 +13,14 @@
  notice, this list of conditions and the following disclaimer in the
  documentation and/or other materials provided with the distribution.
  
- * Neither the name of zuui.org nor the names of its contributors may 
+ * Neither the name of Philip Kluz, 'zuui.org' nor the names of its contributors may 
  be used to endorse or promote products derived from this software 
  without specific prior written permission.
  
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL ZUUI.ORG BE LIABLE FOR ANY DIRECT, 
+ DISCLAIMED. IN NO EVENT SHALL PHILIP KLUZ BE LIABLE FOR ANY DIRECT, 
  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
