e7fbae7da365df395ac15ddc64d4f2cd601db83f
Philip Kluz <pkluz@Philips-MacBook-Pro-2.local>
2012-12-24 17:16:47 +0100

    Initial Commit

---
+++ PKRevealController/PKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PKAppDelegate.h
+//  PKRevealController
+//
+//  Created by Philip Kluz on 12/24/12.
+//  Copyright (c) 2012 zuui.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
