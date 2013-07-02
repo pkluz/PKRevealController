619172ce52064f21b7b28f38f9995c41c95de459
justinb <justinb@glam.com>
2013-07-02 14:47:44 -0700

    Specify delegate as readwrite to conform with other property declarations.

--- PKRevealController/Controller/PKRevealController.h
+++ PKRevealController/Controller/PKRevealController.h
@@ -135,7 +135,7 @@ typedef void(^PKDefaultErrorHandler)(NSError *error);
 @property (nonatomic, assign, readwrite) BOOL disablesFrontViewInteraction;
 @property (nonatomic, assign, readwrite) BOOL recognizesPanningOnFrontView;
 @property (nonatomic, assign, readwrite) BOOL recognizesResetTapOnFrontView;
-@property (nonatomic, assign) id <PKRevealDelegate>delegate;
+@property (nonatomic, assign, readwrite) id <PKRevealDelegate>delegate;
 
 #pragma mark - Methods
 
