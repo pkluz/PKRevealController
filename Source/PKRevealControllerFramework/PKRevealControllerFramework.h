0f5bfd08677b3ef3abe5917307ae4cfccbe4020a
Juan Fernández Sagasti <juan@theagilemonkeys.com>
2016-03-23 09:43:56 +0000

    + Added Carthage support

---
+++ Source/PKRevealControllerFramework/PKRevealControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  PKRevealControllerFramework.h
+//  PKRevealControllerFramework
+//
+//  Created by Juan Fernández Sagasti on 23/3/16.
+//  Copyright © 2016 zuui.org (Philip Kluz). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PKRevealControllerFramework.
+FOUNDATION_EXPORT double PKRevealControllerFrameworkVersionNumber;
+
+//! Project version string for PKRevealControllerFramework.
+FOUNDATION_EXPORT const unsigned char PKRevealControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKRevealControllerFramework/PublicHeader.h>
+
+#import <PKRevealControllerFramework/PKRevealController.h>
